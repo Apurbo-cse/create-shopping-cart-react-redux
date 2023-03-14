455517e46947613d8189d59efa50881d23eb2a2c
Apurbo Kumar Anup <apurboka@gmail.com>
2023-03-14 12:50:27 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 
+import { Provider } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import Navbar from './layouts/include/Navbar';
 import MasterLayout from './layouts/MasterLayout';
+import store from './redux/store';
 
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <Navbar/>
       <Routes>
         <Route path="/" element={<MasterLayout />}>
           <Route index path="/" element={<Home />} />
         </Route>
       </Routes>
-    </>
+    </Provider>
   );
 }
 
