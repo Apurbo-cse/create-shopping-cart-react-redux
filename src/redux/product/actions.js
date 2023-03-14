797b42395b8bb59de6337cf36af30606d91b6c85
Apurbo Kumar Anup <apurboka@gmail.com>
2023-03-14 17:13:50 +0600

    Update actions.js

--- src/redux/product/actions.js
+++ src/redux/product/actions.js
@@ -1,12 +1,13 @@
 import { ADDED } from "./actionTypes";
 
-export const added = (title, category, image, qty) => {
+export const added = (title, category, image, price, qty) => {
   return {
     type: ADDED,
     payload: {
       title,
       category,
       image,
+      price,
       qty,
     },
   };
