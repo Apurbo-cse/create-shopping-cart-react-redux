f7a48f4312c934b44efc3b071f88a2911f596a24
Apurbo Kumar Anup <apurboka@gmail.com>
2023-03-14 15:00:28 +0600

    Update ProductCard.js

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -8,8 +8,12 @@ const ProductCard = ({product}) => {
     <div class="col-md-3 mb-3">
       <div class="card w-100 p-3">
         <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"/>
-        <p>{title}</p>
-        <button class="btn btn-danger">Add to cart</button>
+        <p className="mb-0">{title}</p>
+        <div className="d-flex justify-content-between align-items-center mt-0">
+          <div><small className="text-muted">Men Shoes</small></div>
+          <div><small className="text-muted">QTY 10</small></div>
+        </div>
+        <button class="btn btn-danger mt-2">Add to cart</button>
       </div>
     </div>
   </>
