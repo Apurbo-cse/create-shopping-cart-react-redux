2fdbac7a85cf9d78f46279e6b2f4ae52e73c333a
Apurbo Kumar Anup <apurboka@gmail.com>
2023-03-14 17:13:13 +0600

    Update initialState.js

--- src/redux/product/initialState.js
+++ src/redux/product/initialState.js
@@ -4,6 +4,7 @@ const initialState = [
         title : 'First Item',
         category : 'First Category',
         image:'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+        price:100,
         qty:10,
     }
 ]
