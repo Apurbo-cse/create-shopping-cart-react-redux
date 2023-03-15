6d1c79a1daaaabd32f21518b5cd513a2f0c96fbb
Apurbo Kumar Anup <apurboka@gmail.com>
2023-03-15 12:53:50 +0600

    Update CartItem.js

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,35 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { removeFromCart } from "../redux/product/actions";
+import { removeFromCart, increment, decrement } from "../redux/product/actions";
 
 const CartItem = ({ product }) => {
   const dispatch = useDispatch();
+  const [quantity, setQuantity] = useState(product.cartQty);
 
   const handleRemoveFromCart = () => {
     dispatch(removeFromCart(product.id));
   };
 
+  const handleIncrement = () => {
+    dispatch(increment(product.id));
+    setQuantity(quantity + 1);
+  };
+
+  
+
+  const handleDecrement = () => {
+    if (quantity > 1) {
+      dispatch(decrement(product.id));
+      setQuantity(quantity - 1);
+    }
+  };
+
   return (
     <>
-    
       <div className="row d-flex align-items-center border-2 shadow mb-2 p-0">
-        <div className="col-md-2 p-2"><img src={product.image} alt={product.title} width={120}/></div>
+        <div className="col-md-2 p-2">
+          <img src={product.image} alt={product.title} width={120} />
+        </div>
         <div className="col-md-3">
-            <p className="m-0 px-2 text-muted">{product.title}</p>
-            <small className="m-0 px-2 text-muted">{product.category}</small>
+          <p className="m-0 px-2 text-muted">{product.title}</p>
+          <small className="m-0 px-2 text-muted">{product.category}</small>
         </div>
         <div className="col-md-4 ">
-            <button className="btn mx-2">-</button><span className="border-1 border-warning bg-info p-2 ">10</span><button className="btn mx-2">+</button>
+          <span className="btn mx-2 btn-danger py-1  " onClick={handleDecrement}>-</span>
+          <span className="border-1 border-warning p-2 ">{quantity}</span>
+          <span className="btn mx-2 btn-success py-1" onClick={handleIncrement}>+</span>
         </div>
         <div className="col-md-2 ">
-        <p className="m-0 px-2 text-muted">{product.price}</p>
+          <p className="m-0 px-2 text-muted">{product.price * quantity}</p>
         </div>
         <div className="col-md-1">
-        <button className="btn btn-danger" onClick={handleRemoveFromCart}>
-        <i class="fa-solid fa-trash"></i>
-          </button>
+          <span className="btn btn-danger" onClick={handleRemoveFromCart}>
+            <i className="fa-solid fa-trash"></i>
+          </span>
         </div>
-
       </div>
     </>
   );
